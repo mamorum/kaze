ab95533dac43ac995ab37faf1bebcf99f31ec18a
mamorum <mamoru310@yahoo.com>
2016-09-05 13:55:04 +0900

    Reduce the time calling Class#newInstance();

--- src/main/java/kaze/fw/Func.java
+++ src/main/java/kaze/fw/Func.java
@@ -2,25 +2,34 @@ package kaze.fw;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 import kaze.http.Req;
 import kaze.http.Res;
 
 public class Func {
 
+  private static Map<String, Object> cache = new HashMap<>();
+  
   Method m; Object o;
   private Func(Method m, Object o) {
     this.m = m; this.o = o;
   }
 
-  // TODO インスタンス化を減らす方法を検討する。
-  // メソッドとインスタンスが１：１になっている。
   public static Func of(Method m) {
-     try {
-       return new Func(
-         m, m.getDeclaringClass()
-                 .newInstance()
-         );
+    Class<?> c = m.getDeclaringClass();
+    String n = c.getName();
+    Object o = cache.get(n);
+    if (o == null) o = create(n, c); 
+    return new Func(m, o);
+  }
+  
+  public static Object create(String n, Class<?> c) {
+    try {
+      Object o = c.newInstance();
+      cache.put(n, o);
+      return o;
     } catch (
       InstantiationException |
       IllegalAccessException e
