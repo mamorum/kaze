52187e2f3b5b0d85d3fb80f52906bedbbf010333
mamorum <mamorum@users.noreply.github.com>
2018-04-10 12:48:40 +0900

    Update encoding test.

--- src/test/java/kaze/AppJettyTest.java
+++ src/test/java/kaze/AppJettyTest.java
@@ -12,61 +12,6 @@ public class AppJettyTest {
   @BeforeClass public static void init() {
     JettyEnv.init();
   }
-  //-> encoding test
-  @Test public void enc_enable() {
-    // pre
-    App app = new App();
-    app.get("/", (req, res) -> {
-      res.write("text/plain", "Hi");
-    });
-    Jetty.app(app , "/enc-enable/*");
-    // test
-    HttpRes res = HttpReq.get(
-      "http://localhost:8080/enc-enable"
-    );
-    // check
-    res.typeIs("text/plain;charset=utf-8");
-    res.statusIs(200);
-    res.bodyIs("Hi");
-    res.close();
-  }
-  @Test public void enc_disable() {
-    // pre
-    App app = new App();
-    app.disableEncoding();
-    app.get("/", (req, res) -> {
-      res.write("text/plain", "Hi");
-    });
-    Jetty.app(app , "/enc-disable/*");
-    // test
-    HttpRes res = HttpReq.get(
-      "http://localhost:8080/enc-disable"
-    );
-    // check
-    /// iso-... is jetty's default charset for text/plain.
-    res.typeIs("text/plain;charset=iso-8859-1");
-    res.statusIs(200);
-    res.bodyIs("Hi");
-    res.close();
-  }
-  @Test public void enc_changed() {
-    // pre
-    App app = new App();
-    app.encoding("utf-16");
-    app.get("/", (req, res) -> {
-      res.write("text/plain", "Hi");
-    });
-    Jetty.app(app , "/enc-changed/*");
-    // test
-    HttpRes res = HttpReq.get(
-      "http://localhost:8080/enc-changed"
-    );
-    // check
-    res.typeIs("text/plain;charset=utf-16");
-    res.statusIs(200);
-    res.bodyIs("Hi");
-    res.close();
-  }
   //-> run test
   @Test public void run_not_found() {
     // pre
---
+++ src/test/java/kaze/EncJettyTest.java
@@ -0,0 +1,109 @@
+package kaze;
+
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import kaze.server.Jetty;
+import tools.HttpReq;
+import tools.HttpRes;
+import tools.JettyEnv;
+
+public class EncJettyTest {
+  @BeforeClass public static void init() {
+    JettyEnv.init();
+  }
+  //-> encoding test
+  @Test public void enc_enable() {
+    // pre
+    App app = new App();
+    app.get("/", (req, res) -> {
+      res.write("text/plain", "Hi");
+    });
+    Jetty.app(app , "/enc-enable/*");
+    // test
+    HttpRes res = HttpReq.get(
+      "http://localhost:8080/enc-enable"
+    );
+    // check
+    res.typeIs("text/plain;charset=utf-8");
+    res.statusIs(200);
+    res.bodyIs("Hi");
+    res.close();
+  }
+  @Test public void enc_disable() {
+    // pre
+    App app = new App();
+    app.disableEncoding();
+    app.get("/", (req, res) -> {
+      res.write("text/plain", "Hi");
+    });
+    Jetty.app(app , "/enc-disable/*");
+    // test
+    HttpRes res = HttpReq.get(
+      "http://localhost:8080/enc-disable"
+    );
+    // check
+    /// iso-... is jetty's default charset for text/plain.
+    res.typeIs("text/plain;charset=iso-8859-1");
+    res.statusIs(200);
+    res.bodyIs("Hi");
+    res.close();
+  }
+  @Test public void enc_changed() {
+    // pre
+    App app = new App();
+    app.encoding("utf-16");
+    app.get("/", (req, res) -> {
+      res.write("text/plain", "Hi");
+    });
+    Jetty.app(app , "/enc-changed/*");
+    // test
+    HttpRes res = HttpReq.get(
+      "http://localhost:8080/enc-changed"
+    );
+    // check
+    res.typeIs("text/plain;charset=utf-16");
+    res.statusIs(200);
+    res.bodyIs("Hi");
+    res.close();
+  }
+  @Test public void enc_filter() {
+    // pre
+    App app = new App();
+    app.disableEncoding();
+    app.get("/on", (req, res) -> {
+      res.write("text/plain", "on");
+    });
+    app.get("/off", (req, res) -> {
+      res.write("text/plain", "off");
+    });
+    Jetty.app(app , "/enc-filter/*");
+    FilterHolder f = new FilterHolder(new Enc());
+    f.setInitParameter("encoding", "utf-8");
+    Jetty.context.addFilter(
+      f, "/enc-filter/on", EnumSet.of(DispatcherType.REQUEST)
+    );
+    HttpRes res;
+    // on -> test, check
+    res = HttpReq.get(
+      "http://localhost:8080/enc-filter/on"
+    );
+    res.typeIs("text/plain;charset=utf-8");
+    res.statusIs(200);
+    res.bodyIs("on");
+    res.close();
+    // off -> test, check
+    res = HttpReq.get(
+      "http://localhost:8080/enc-filter/off"
+    );
+    res.typeIs("text/plain;charset=iso-8859-1");
+    res.statusIs(200);
+    res.bodyIs("off");
+    res.close();
+  }
+}
